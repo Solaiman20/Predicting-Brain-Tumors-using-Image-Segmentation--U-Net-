a988bf8ce3afec43e2a03930a2cd7044245b0206
Solaiman AlDokhail <ssaldokhail@gmail.com>
2025-02-07 04:09:36 +0300

    Training and hypertuning

---
+++ u_net.py
@@ -0,0 +1,404 @@
+# %%
+import tensorflow as tf
+# Check for available GPUs and enable memory growth to avoid allocation errors.
+gpus = tf.config.list_physical_devices('MPS')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        print("CUDA-enabled GPU(s) detected and configured.")
+    except RuntimeError as e:
+        print("Error configuring GPU: ", e)
+
+# %%
+import numpy as np
+import pandas as pd
+import os
+from PIL import Image
+import glob
+import json
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import concatenate
+from pycocotools.coco import COCO
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras import backend as K
+
+# %%
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("pkdarabi/brain-tumor-image-dataset-semantic-segmentation")
+
+print("Path to dataset files:", path)
+
+# %%
+train_dir = path + '/train'
+val_dir = path + '/valid'
+test_dir = path + '/test'
+
+train_annotation_file = train_dir + '/_annotations.coco.json'
+test_annotation_file = test_dir + '/_annotations.coco.json'
+val_annotation_file = val_dir + '/_annotations.coco.json'
+
+# %%
+train_coco = COCO(train_annotation_file)
+val_coco = COCO(val_annotation_file)
+test_coco = COCO(test_annotation_file)
+
+# %%
+def load_image_and_mask(coco, image_dir, image_id):
+    image_info = coco.loadImgs(image_id)[0]
+    image_path = os.path.join(image_dir, image_info['file_name'])
+    image = Image.open(image_path)
+    image = np.array(image)
+
+    ann_ids = coco.getAnnIds(imgIds=image_id)
+    anns = coco.loadAnns(ann_ids)
+    mask = np.zeros((image_info['height'], image_info['width']))
+    for ann in anns:
+        mask = np.maximum(mask, coco.annToMask(ann))
+
+    return image, mask
+
+# %%
+def create_tf_dataset(coco, image_dir, image_ids):
+    def generator():
+        for image_id in image_ids:
+            yield load_image_and_mask(coco, image_dir, image_id)
+
+    return tf.data.Dataset.from_generator(generator,
+                                          output_signature=(tf.TensorSpec(shape=(None, None, 3), dtype=tf.uint8),
+                                                            tf.TensorSpec(shape=(None, None), dtype=tf.uint8)))
+
+train_dataset = create_tf_dataset(train_coco, train_dir, train_coco.getImgIds())
+val_dataset = create_tf_dataset(val_coco, val_dir, val_coco.getImgIds())
+test_dataset = create_tf_dataset(test_coco, test_dir, test_coco.getImgIds())
+
+# %%
+def preprocess(image, mask):
+
+    image = tf.image.resize(image, (256, 256))
+
+    mask = tf.expand_dims(mask, axis=-1)
+    mask = tf.image.resize(mask, (256, 256))
+
+    image = tf.cast(image, tf.float32) / 255.0
+
+    return image, mask
+
+train_dataset = train_dataset.map(preprocess)
+val_dataset = val_dataset.map(preprocess)
+test_dataset = test_dataset.map(preprocess)
+
+# %%
+def visualize_dataset(dataset, num_samples=5):
+    for i, (image, mask) in enumerate(dataset.take(num_samples)):
+        plt.figure(figsize=(10, 5))
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(image.numpy())
+        plt.title("Image")
+        plt.axis("off")
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(mask.numpy().squeeze(), cmap="gray")
+        plt.title("Mask")
+        plt.axis("off")
+
+        plt.show()
+
+visualize_dataset(train_dataset)
+visualize_dataset(val_dataset)
+
+# %%
+# UNQ_C1
+# GRADED FUNCTION: conv_block
+def conv_block(inputs=None, n_filters=32, dropout_prob=0, max_pooling=True):
+    """
+    Convolutional downsampling block
+
+    Arguments:
+        inputs -- Input tensor
+        n_filters -- Number of filters for the convolutional layers
+        dropout_prob -- Dropout probability
+        max_pooling -- Use MaxPooling2D to reduce the spatial dimensions of the output volume
+    Returns:
+        next_layer, skip_connection --  Next layer and skip connection outputs
+    """
+
+    conv = Conv2D(n_filters, # Number of filters
+                  (3,3),   # Kernel size
+                  activation="relu",
+                  padding="same",
+                  kernel_initializer='he_normal')(inputs)
+    conv = Conv2D(n_filters, # Number of filters
+                  (3,3),   # Kernel size
+                  activation="relu",
+                  padding="same",
+                  # set 'kernel_initializer' same as above
+                  kernel_initializer="he_normal")(conv)
+
+    # if dropout_prob > 0 add a dropout layer, with the variable dropout_prob as parameter
+    if dropout_prob > 0:
+         ### START CODE HERE
+        conv = Dropout(dropout_prob)(conv)
+         ### END CODE HERE
+
+
+    # if max_pooling is True add a MaxPooling2D with 2x2 pool_size
+    if max_pooling:
+        ### START CODE HERE
+        next_layer = MaxPooling2D((2,2))(conv)
+        ### END CODE HERE
+
+    else:
+        next_layer = conv
+
+    skip_connection = conv
+
+    return next_layer, skip_connection
+
+# %%
+# UNQ_C2
+# GRADED FUNCTION: upsampling_block
+def upsampling_block(expansive_input, contractive_input, n_filters=32):
+    """
+    Convolutional upsampling block
+
+    Arguments:
+        expansive_input -- Input tensor from previous layer
+        contractive_input -- Input tensor from previous skip layer
+        n_filters -- Number of filters for the convolutional layers
+    Returns:
+        conv -- Tensor output
+    """
+
+    up = Conv2DTranspose(
+                 n_filters,    # number of filters
+                 (3,3),    # Kernel size
+                 strides=(2,2),
+                 padding="same")(expansive_input)
+
+    # Merge the previous output and the contractive_input
+    merge = concatenate([up, contractive_input], axis=3)
+    conv = Conv2D(n_filters,   # Number of filters
+                 (3,3),     # Kernel size
+                 activation="relu",
+                 padding="same",
+                 kernel_initializer='he_normal')(merge)
+    conv = Conv2D(n_filters,   # Number of filters
+                 (3,3),     # Kernel size
+                 activation="relu",
+                 padding="same",
+                 kernel_initializer='he_normal')(conv)
+
+    return conv
+
+# %%
+# UNQ_C3
+# GRADED FUNCTION: unet_model
+def unet_model(input_size=(256, 256, 3), n_filters=32, n_classes=1):
+    """
+    Unet model
+
+    Arguments:
+        input_size -- Input shape
+        n_filters -- Number of filters for the convolutional layers
+        n_classes -- Number of output classes
+    Returns:
+        model -- tf.keras.Model
+    """
+    inputs = Input(input_size)
+    # Contracting Path (encoding)
+    # Add a conv_block with the inputs of the unet_ model and n_filters
+    ### START CODE HERE
+    cblock1 = conv_block(inputs, n_filters)
+    # Chain the first element of the output of each block to be the input of the next conv_block.
+    # Double the number of filters at each new step
+    cblock2 = conv_block(cblock1[0], 2*n_filters)
+    cblock3 = conv_block(cblock2[0], 4*n_filters)
+    cblock4 = conv_block(cblock3[0], 8*n_filters, dropout_prob=0.3) # Include a dropout_prob of 0.3 for this layer
+    # Include a dropout_prob of 0.3 for this layer, and avoid the max_pooling layer
+    cblock5 = conv_block(cblock4[0], 16*n_filters, dropout_prob=0.3, max_pooling=False)
+    ### END CODE HERE
+
+    # Expanding Path (decoding)
+    # Add the first upsampling_block.
+    # Use the cblock5[0] as expansive_input and cblock4[1] as contractive_input and n_filters * 8
+    ### START CODE HERE
+    ublock6 = upsampling_block(cblock5[0], cblock4[1],  n_filters * 8)
+    # Chain the output of the previous block as expansive_input and the corresponding contractive block output.
+    # Note that you must use the second element of the contractive block i.e before the maxpooling layer.
+    # At each step, use half the number of filters of the previous block
+    ublock7 = upsampling_block(ublock6, cblock3[1],  n_filters * 4)
+    ublock8 = upsampling_block(ublock7, cblock2[1],  n_filters * 2)
+    ublock9 = upsampling_block(ublock8, cblock1[1],  n_filters)
+    ### END CODE HERE
+
+    conv9 = Conv2D(n_filters,
+                 3,
+                 activation='relu',
+                 padding='same',
+                 # set 'kernel_initializer' same as above exercises
+                 kernel_initializer='he_normal')(ublock9)
+
+
+    # Add a Conv2D layer with n_classes filter, kernel size of 1 and a 'same' padding
+    ### START CODE HERE
+    conv10 = Conv2D(n_classes, (1,1), padding="same", activation='sigmoid')(conv9)
+    ### END CODE HERE
+
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
+
+    return model
+
+# %%
+model = unet_model(input_size=(256, 256, 3), n_filters=32)
+
+
+# %%
+def dice_coef(y_true, y_pred, smooth=1e-6):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coef(y_true, y_pred)
+
+def combined_loss(y_true, y_pred):
+    dice = dice_loss(y_true, y_pred)
+    bce = tf.keras.losses.binary_crossentropy(y_true, y_pred)
+    return 0.65 * dice + 0.35 * bce
+def f1_score(y_true, y_pred):
+    """
+    Computes the F1 score, the harmonic mean of precision and recall.
+    Assumes binary predictions (using a threshold of 0.5).
+    """
+    # Convert probabilities to binary predictions
+    y_pred_bin = tf.cast(tf.greater(y_pred, 0.5), tf.float32)
+    
+    # Calculate True Positives (TP), False Positives (FP) and False Negatives (FN)
+    tp = tf.reduce_sum(y_true * y_pred_bin)
+    fp = tf.reduce_sum((1 - y_true) * y_pred_bin)
+    fn = tf.reduce_sum(y_true * (1 - y_pred_bin))
+    
+    # Calculate precision and recall with a small epsilon to avoid division by zero
+    precision = tp / (tp + fp + K.epsilon())
+    recall = tp / (tp + fn + K.epsilon())
+    
+    # Compute the F1 score
+    f1 = 2 * precision * recall / (precision + recall + K.epsilon())
+    return f1
+
+metrics = ["accuracy", dice_coef, f1_score]
+
+# Compile model
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+model.compile(optimizer=optimizer, loss=combined_loss, metrics=metrics)
+model.summary()
+
+# %%
+BATCH_SIZE = 16
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_dataset = train_dataset.repeat().batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
+val_dataset = val_dataset.repeat().batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
+
+steps_per_epoch = len(train_coco.getImgIds()) // BATCH_SIZE
+validation_steps = len(val_coco.getImgIds()) // BATCH_SIZE
+test_steps = len(test_coco.getImgIds()) // BATCH_SIZE
+
+# %%
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4, min_lr=1e-5)
+early_stopping = EarlyStopping(monitor='val_loss', patience=8, restore_best_weights=True)
+
+history = model.fit(
+    train_dataset,
+    validation_data=val_dataset,
+    epochs=6,
+    steps_per_epoch=steps_per_epoch,
+    validation_steps=validation_steps,
+    callbacks=[early_stopping, reduce_lr]
+)
+
+# %%
+def plot_f1(history):
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['f1_score'], label='Training F1 Score')
+    plt.plot(history.history['val_f1_score'], label='Validation F1 Score')
+    plt.title('Training and Validation F1 Score')
+    plt.xlabel('Epochs')
+    plt.ylabel('F1 Score')
+    plt.legend()
+def plot_loss(history):
+    plt.figure(figsize=(12, 6))
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+def plot_accuracy(history):
+    plt.figure(figsize=(12, 6))
+    plt.plot(history.history['accuracy'], label='Training Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+plot_loss(history)
+plot_accuracy(history)
+plot_f1(history)
+
+# %%
+test_loss, test_accuracy, test_coef, test_f1 = model.evaluate(test_dataset, steps=test_steps)
+print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}, Test Dice Coefficient: {test_coef}, Test F1 Score: {test_f1}")
+
+# %%
+import random
+def visualize_predictions(model, dataset, num_samples=5, threshold=0.5):
+
+    random_batch = random.choice(list(dataset))
+    images, masks = random_batch
+
+    indices = random.sample(range(len(images)), min(num_samples, len(images)))
+
+    predictions = model.predict(images)
+
+    binary_predictions = (predictions > threshold).astype('uint8')
+
+    for i in indices:
+        plt.figure(figsize=(15, 5))
+
+        plt.subplot(1, 3, 1)
+        plt.imshow(images[i])
+        plt.title('Original Image')
+        plt.axis('off')
+        plt.subplot(1, 3, 2)
+        plt.imshow(masks[i].numpy().squeeze(), cmap='gray')
+        plt.title('True Mask')
+        plt.axis('off')
+
+        plt.subplot(1, 3, 3)
+        plt.imshow(binary_predictions[i].squeeze(), cmap='gray')
+        plt.title('Predicted Mask')
+        plt.axis('off')
+
+        plt.savefig(f"figure_{i}.png")
+        plt.close()
+
+visualize_predictions(model, test_dataset, num_samples=10)
+model.save("unet_tumor_model.keras")
+
+
